89b22c126b0eb7b39fd0fdc595447f3485db340a
Aayush Pal <ap.aayushpal@gmail.com>
2020-09-29 11:06:01 +0530

    refactored with slices

--- arraysnslices/arraynslices_test.go
+++ arraysnslices/arraynslices_test.go
@@ -3,25 +3,14 @@ package arraysnslices
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("--- Collection of 5 definite numbers", func(t *testing.T) {
-		numbers := []int{1, 2, 3, 4, 5}
 
-		got := Sum(numbers)
-		want := 15
+	numbers := []int{12, 241, 64, 76, 89, 51}
 
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
+	got := Sum(numbers)
+	want := 533
 
-	t.Run("--- Collection of undefined numbers", func(t *testing.T) {
-		numbers := []int{12, 241, 64, 76, 89, 51}
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
 
-		got := Sum(numbers)
-		want := 533
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
-	})
 }
